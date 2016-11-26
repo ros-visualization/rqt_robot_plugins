79f0535524963aed11f4804d0c876244f42063e8
DorianScholz <dorianscholz@gmx.de>
2016-11-26 20:55:46 +0100

    rqt_pose_view: search through message classes for Pose, Quaternion or Point data

--- rqt_pose_view/src/rqt_pose_view/pose_view_widget.py
+++ rqt_pose_view/src/rqt_pose_view/pose_view_widget.py
@@ -38,9 +38,12 @@ from python_qt_binding.QtWidgets import QAction, QMenu, QWidget
 
 import rospy
 from rostopic import get_topic_class
+from rqt_py_common.topic_helpers import find_slots_by_type_bfs
 from tf.transformations import quaternion_matrix, quaternion_about_axis
+from geometry_msgs.msg import Quaternion, Pose, Point
 
-from OpenGL.GL import glBegin, glColor3f, glEnd, glLineWidth, glMultMatrixf, glTranslatef, glVertex3f, GL_LINES, GL_QUADS
+from OpenGL.GL import glBegin, glColor3f, glEnd, glLineWidth, glMultMatrixf, glTranslatef, \
+    glVertex3f, GL_LINES, GL_QUADS
 from .gl_widget import GLWidget
 
 
@@ -54,7 +57,7 @@ class PoseViewWidget(QWidget):
         loadUi(ui_file, self)
         self._plugin = plugin
 
-        self._position = (0.0, 0.0, 0.0)
+        self._position = (2.0, 2.0, 2.0)
         self._orientation = quaternion_about_axis(0.0, (1.0, 0.0, 0.0))
         self._topic_name = None
         self._subscriber = None
@@ -99,16 +102,8 @@ class PoseViewWidget(QWidget):
         self._gl_view.makeCurrent()
         self._gl_view.reset_view()
         self._gl_view.rotate((0, 0, 1), 45)
-        self._gl_view.rotate((1, 0, 0), -45)
+        self._gl_view.rotate((1, 0, 0), -65)
         self._gl_view.translate((0, -3, -15))
-
-    def message_callback_pose(self, message):
-        self._position = (message.position.x, message.position.y, message.position.z)
-        self._orientation = (message.orientation.x, message.orientation.y, message.orientation.z, message.orientation.w)
-            
-    def message_callback_quaternion(self, message):
-        self._position = (0, 0, 0)
-        self._orientation = (message.x, message.y, message.z, message.w)
 
     def update_timeout(self):
         self._gl_view.makeCurrent()
@@ -121,7 +116,9 @@ class PoseViewWidget(QWidget):
         self._paintGLBox()
 
     def _paintGLBox(self):
+        # FIXME: add user configurable setting to allow use of translation as well
         self._position = (2.0, 2.0, 2.0)  # Set fixed translation for now
+
         glTranslatef(*self._position)     # Translate Box
 
         matrix = quaternion_matrix(self._orientation)  # convert quaternion to translation matrix
@@ -167,8 +164,8 @@ class PoseViewWidget(QWidget):
         glEnd()                           # Done Drawing The Quad
 
     def _paintGLGrid(self):
-        resolutionMillimeters = 1
-        griddedAreaSize = 100
+        resolution_millimeters = 1
+        gridded_area_size = 100
 
         glLineWidth(1.0)
 
@@ -176,23 +173,23 @@ class PoseViewWidget(QWidget):
 
         glColor3f(1.0, 1.0, 1.0)
 
-        glVertex3f(griddedAreaSize, 0, 0)
-        glVertex3f(-griddedAreaSize, 0, 0)
-        glVertex3f(0, griddedAreaSize, 0)
-        glVertex3f(0, -griddedAreaSize, 0)
+        glVertex3f(gridded_area_size, 0, 0)
+        glVertex3f(-gridded_area_size, 0, 0)
+        glVertex3f(0, gridded_area_size, 0)
+        glVertex3f(0, -gridded_area_size, 0)
 
-        numOfLines = int(griddedAreaSize / resolutionMillimeters)
+        num_of_lines = int(gridded_area_size / resolution_millimeters)
 
-        for i in range(numOfLines):
-            glVertex3f(resolutionMillimeters * i, -griddedAreaSize, 0)
-            glVertex3f(resolutionMillimeters * i, griddedAreaSize, 0)
-            glVertex3f(griddedAreaSize, resolutionMillimeters * i, 0)
-            glVertex3f(-griddedAreaSize, resolutionMillimeters * i, 0)
+        for i in range(num_of_lines):
+            glVertex3f(resolution_millimeters * i, -gridded_area_size, 0)
+            glVertex3f(resolution_millimeters * i, gridded_area_size, 0)
+            glVertex3f(gridded_area_size, resolution_millimeters * i, 0)
+            glVertex3f(-gridded_area_size, resolution_millimeters * i, 0)
 
-            glVertex3f(resolutionMillimeters * (-i), -griddedAreaSize, 0)
-            glVertex3f(resolutionMillimeters * (-i), griddedAreaSize, 0)
-            glVertex3f(griddedAreaSize, resolutionMillimeters * (-i), 0)
-            glVertex3f(-griddedAreaSize, resolutionMillimeters * (-i), 0)
+            glVertex3f(resolution_millimeters * (-i), -gridded_area_size, 0)
+            glVertex3f(resolution_millimeters * (-i), gridded_area_size, 0)
+            glVertex3f(gridded_area_size, resolution_millimeters * (-i), 0)
+            glVertex3f(-gridded_area_size, resolution_millimeters * (-i), 0)
 
         glEnd()
 
@@ -225,16 +222,36 @@ class PoseViewWidget(QWidget):
 
     @Slot('QDragEnterEvent*')
     def dragEnterEvent(self, event):
-        if not event.mimeData().hasText():
+        if event.mimeData().hasText():
+            topic_name = str(event.mimeData().text())
+            if len(topic_name) == 0:
+                qWarning('PoseViewWidget.dragEnterEvent(): event.mimeData() text is empty')
+                return
+        else:
             if not hasattr(event.source(), 'selectedItems') or len(event.source().selectedItems()) == 0:
-                qWarning('Plot.dragEnterEvent(): not hasattr(event.source(), selectedItems) or len(event.source().selectedItems()) == 0')
+                qWarning('PoseViewWidget.dragEnterEvent(): event.source() has no attribute selectedItems or length of selectedItems is 0')
                 return
             item = event.source().selectedItems()[0]
-            ros_topic_name = item.data(0, Qt.UserRole)
-            if ros_topic_name is None:
-                qWarning('Plot.dragEnterEvent(): not hasattr(item, ros_topic_name_)')
+            topic_name = item.data(0, Qt.UserRole)
+
+            if topic_name is None:
+                qWarning('PoseViewWidget.dragEnterEvent(): selectedItem has no UserRole data with a topic name')
                 return
-        # TODO: do some checks for valid topic here
+
+        # check for valid topic
+        msg_class, self._topic_name, _ = get_topic_class(topic_name)
+        if msg_class is None:
+            qWarning('PoseViewWidget.dragEnterEvent(): No message class was found for topic "%s".' % topic_name)
+            return
+
+        # check for valid message class
+        quaternion_slot_path, point_slot_path = self._get_slot_paths(msg_class)
+
+        if quaternion_slot_path is None and point_slot_path is None:
+            qWarning('PoseViewWidget.dragEnterEvent(): No Pose, Quaternion or Point data was found outside of arrays in "%s" on topic "%s".'
+                     % (msg_class._type, topic_name))
+            return
+
         event.acceptProposedAction()
 
     @Slot('QDropEvent*')
@@ -242,22 +259,84 @@ class PoseViewWidget(QWidget):
         if event.mimeData().hasText():
             topic_name = str(event.mimeData().text())
         else:
-            droped_item = event.source().selectedItems()[0]
-            topic_name = str(droped_item.data(0, Qt.UserRole))
+            dropped_item = event.source().selectedItems()[0]
+            topic_name = str(dropped_item.data(0, Qt.UserRole))
 
-        self.unregister_topic()
-        self.subscribe_topic(topic_name)
+        self._unregister_topic()
+        self._subscribe_topic(topic_name)
 
-    def unregister_topic(self):
+    def _unregister_topic(self):
         if self._subscriber:
             self._subscriber.unregister()
 
-    def subscribe_topic(self, topic_name):
+    @staticmethod
+    def _make_path_list_from_path_string(path):
+        path = path.split('/')
+        if path == ['']:
+            return []
+        return path
+
+    @staticmethod
+    def _get_slot_paths(msg_class):
+        # find first Pose in msg_class
+        pose_slot_paths = find_slots_by_type_bfs(msg_class, Pose)
+        for path in pose_slot_paths:
+            # make sure the path does not contain an array, because we don't want to deal with empty arrays...
+            if '[' not in path:
+                path = PoseViewWidget._make_path_list_from_path_string(pose_slot_paths[0])
+                return path + ['orientation'], path + ['position']
+
+        # if no Pose is found, find first Quaternion and Point
+        quaternion_slot_paths = find_slots_by_type_bfs(msg_class, Quaternion)
+        for path in quaternion_slot_paths:
+            if '[' not in path:
+                quaternion_slot_path = PoseViewWidget._make_path_list_from_path_string(path)
+                break
+        else:
+            quaternion_slot_path = None
+
+        point_slot_paths = find_slots_by_type_bfs(msg_class, Point)
+        for path in point_slot_paths:
+            if '[' not in path:
+                point_slot_path = PoseViewWidget._make_path_list_from_path_string(path)
+                break
+        else:
+            point_slot_path = None
+
+        return quaternion_slot_path, point_slot_path
+
+
+    def _subscribe_topic(self, topic_name):
         msg_class, self._topic_name, _ = get_topic_class(topic_name)
-        if (msg_class.__name__ == 'Quaternion') :
-            self._subscriber = rospy.Subscriber(self._topic_name, msg_class, self.message_callback_quaternion)
-        else :
-            self._subscriber = rospy.Subscriber(self._topic_name, msg_class, self.message_callback_pose)
-            
+        quaternion_slot_path, point_slot_path = self._get_slot_paths(msg_class)
+
+        self._subscriber = rospy.Subscriber(
+                self._topic_name,
+                msg_class,
+                self.message_callback,
+                callback_args=(quaternion_slot_path, point_slot_path)
+        )
+
+    def message_callback(self, message, slot_paths):
+        quaternion_slot_path = slot_paths[0]
+        point_slot_path = slot_paths[1]
+
+        if quaternion_slot_path is None:
+            self._orientation = quaternion_about_axis(0.0, (1.0, 0.0, 0.0))
+        else:
+            orientation = message
+            for slot_name in quaternion_slot_path:
+                orientation = getattr(orientation, slot_name)
+            self._orientation = (orientation.x, orientation.y, orientation.z, orientation.w)
+
+        if point_slot_path is None:
+            # if no point is given, set it to a fixed offset so the axes can be seen
+            self._position = (2.0, 2.0, 2.0)
+        else:
+            position = message
+            for slot_name in point_slot_path:
+                position = getattr(position, slot_name)
+            self._position = (position.x, position.y, position.z)
+
     def shutdown_plugin(self):
-        self.unregister_topic()
+        self._unregister_topic()
